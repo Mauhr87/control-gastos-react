ee3d847a08ec2deffc374349ca02b0636b08d080
Mauricio Hidalgo Ríos <mauriciohidalgorios@Mauricios-MacBook-Pro.local>
2022-07-15 17:04:04 -0600

    v112

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,66 @@
 import { useState } from 'react'
 import Header from './components/Header'
+import Modal from './components/Modal'
+import ListadoGastos from './components/ListadoGastos'
+import {generateId} from './helpers'
+import IconoNuevoGasto from './img/nuevo-gasto.svg'
 
 function App() {
 
   const [presupuesto, setPresupuesto] = useState(0);
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
 
+  const [modal, setModal] = useState(false);
+  const [animarModal, setAnimarModal] = useState(false);
+
+  const [gastos, setGastos] = useState([]);
+
+
+  const handleNuevoGasto = () => {
+    setModal(true);
+    setTimeout(() => {
+      setAnimarModal(true);
+    }, 300);
+  }
+
+  const guardarGasto = gasto => {
+    gasto.id = generateId();
+    gasto.fecha = Date.now();
+    setGastos([...gastos, gasto])
+  }
+
   return (
-    <div>
+    <div className={modal && 'fijar'}>
       <Header
         presupuesto={presupuesto}
         setPresupuesto={setPresupuesto}
         isValidPresupuesto={isValidPresupuesto}
         setIsValidPresupuesto={setIsValidPresupuesto}
       />
+      {isValidPresupuesto && (
+        <>
+          <main>
+            <ListadoGastos
+              gastos={gastos}
+            />
+          </main>
+          <div className="nuevo-gasto">
+            <img 
+              alt="Icono nuevo gasto"
+              src={IconoNuevoGasto}
+              onClick={handleNuevoGasto}
+            />
+          </div>
+        </>
+      )}
+
+      {modal && <Modal
+        setModal={setModal}
+        animarModal={animarModal}
+        setAnimarModal={setAnimarModal}
+        guardarGasto={guardarGasto}
+      />}
+
     </div>
   )
 }
--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
 
 const ControlPresupuesto = ({presupuesto}) => {
+
+  const formatearCantidad = (cantidad) => {
+    return cantidad.toLocaleString('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    })
+  }
+
   return (
     <div className='contenedor-presupuesto contenedor sombra dos-columnas'>
         <div><p>Gráfica</p></div>
         <div className="contenido-presupuesto">
             <p>
-                <span>Presupuesto: </span> ${presupuesto}
+                <span>Presupuesto: </span> {formatearCantidad(presupuesto)}
+            </p>
+            <p>
+                <span>Disponible: </span> {formatearCantidad(0)}
+            </p>
+            <p>
+                <span>Gastado: </span> {formatearCantidad(0)}
             </p>
         </div>
     </div>
